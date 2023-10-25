7319e3ec275c59d22e7f8e52403d14baeae158d0
Gauravsatii <132814818+Gauravsatii@users.noreply.github.com>
2023-10-25 20:21:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+gsap.from(".developer",{
+    x:-80,
+    opacity:0,
+    duration:1,
+    delay:0.3,
+    
+  
+    
+    })
+gsap.from(".coder",{
+    x:80,
+    opacity:0,
+    duration:1,
+    delay:0.3,
+    
+  
+    
+    })
+    gsap.from(".about-description",{
+        y:80,
+        opacity:0,
+        duration:1,
+        delay:0.3,
+        scrub:3,
+        
+      
+        
+        })
+
+
+
+gsap.from(".hobby-description",{
+    x:-80,
+    opacity:0,
+    duration:1,
+    delay:0.3,
+    scrollTrigger:{
+        trigger:".hobby",
+        scroller:"body",
+        start:"top 30%",
+        end:"top 30%",
+        scrub:3,
+        
+        
+    }
+    
+  
+    
+    })
+
+
+ 
+    gsap.from(".services-description1",{
+        x:-80,
+        opacity:0,
+        duration:1.5,
+        delay:1,
+        
+        scrollTrigger:{
+            trigger:".services-description",
+            scroller:"body",
+            start:"top 70%",
+            end:"top 58%",
+            scrub:3,
+            
+        }
+        
+        })
+    gsap.from(".skills h1",{
+        y:80,
+        opacity:0,
+        duration:1,
+        delay:0.3,
+        
+        scrollTrigger:{
+            trigger:".skills",
+            scroller:"body",
+            start:"top 70%",
+            end:"top 58%",
+            scrub:3,
+            
+        }
+        
+        })
+    gsap.from(".projects h1",{
+        y:80,
+        opacity:0,
+        duration:1,
+        delay:0.3,
+        
+        scrollTrigger:{
+            trigger:".projects",
+            scroller:"body",
+            start:"top 70%",
+            end:"top 58%",
+            scrub:3,
+            
+        }
+        
+        })
+    gsap.from(".services-h1",{
+        y:80,
+        opacity:0,
+        duration:1,
+        delay:0.3,
+        
+        scrollTrigger:{
+            trigger:".services-h1",
+            scroller:"body",
+            start:"top 70%",
+            end:"top 58%",
+            scrub:3,
+            
+        }
+        
+        })
+    gsap.from(".footer h1",{
+        y:80,
+        opacity:0,
+        duration:1,
+        delay:0.3,
+        
+        scrollTrigger:{
+            trigger:".footer",
+            scroller:"body",
+            start:"top 70%",
+            end:"top 58%",
+            scrub:3,
+            
+        }
+        
+        })
+    gsap.from(".p-box1 img",{
+       
+        
+        })
